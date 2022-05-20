38ea4b7faa41935a47a1dfb648264bc452b878f7
jxxtty <jaehee941106@gmail.com>
2022-05-20 10:49:24 +0900

    [6장] 컴포넌트 반복_key속성

--- src/section06/IterationSample.js
+++ src/section06/IterationSample.js
@@ -1,7 +1,7 @@
 
 const IterationSample = () => {
     const names = ['눈사람', '얼음', '눈', '바람'];
-    const nameList = names.map((name, index) => <li id={index}>{name}</li>); // 고유한 값이 없을때만 index값을 key로 사용할 것!! (index를 key로 사용하면 배열이 변경될 때 효율적으로 렌더링하지 못한다)
+    const nameList = names.map((name, index) => <li key={index}>{name}</li>); // 고유한 값이 없을때만 index값을 key로 사용할 것!! (index를 key로 사용하면 배열이 변경될 때 효율적으로 렌더링하지 못한다)
     return <ul>{nameList}</ul>
 }
 
